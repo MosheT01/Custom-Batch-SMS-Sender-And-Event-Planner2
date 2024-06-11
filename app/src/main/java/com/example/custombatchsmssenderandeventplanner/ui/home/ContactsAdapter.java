51cd5c04cca1eb146a723d6f98dda883b1868454
yoavkarp <yoavkarp@gmail.com>
2024-06-11 07:55:00 +0300

    Message Screen

---
+++ app/src/main/java/com/example/custombatchsmssenderandeventplanner/ui/home/ContactsAdapter.java
@@ -0,0 +1,48 @@
+package com.example.custombatchsmssenderandeventplanner.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ViewHolder> {
+
+    final List<ContactListItem> mData;
+
+    public ContactsAdapter(List<ContactListItem> data) {
+        this.mData = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.two_line_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textName.setText(mData.get(position).getPrimaryText());
+        holder.textPhone.setText(mData.get(position).getSecondaryText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textName;
+        TextView textPhone;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            textName = itemView.findViewById(android.R.id.text1);
+            textPhone = itemView.findViewById(android.R.id.text2);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/custombatchsmssenderandeventplanner/ui/home/MessageViewModel.java
@@ -0,0 +1,29 @@
+package com.example.custombatchsmssenderandeventplanner.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Calendar;
+
+
+
+public class MessageViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public MessageViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Hello {contact_name},\nYou have been invited to {event_name}\nDate: {event_date}\nTime:{event_time}\nLocation:{event_location}");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
